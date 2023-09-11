28f8f186e4607fcafefcb9c8d124643ae2ed238a
Xiaoshu Zhao <122313731+xiaoshuzhao@users.noreply.github.com>
2023-09-11 16:11:31 -0700

    Add files via upload

---
+++ frontend/build/precache-manifest.dee66642e173a1b02c4e66e794af328a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b66946a3022a6b760e5acd917751a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8ced5b9ac83722e7885",
+    "url": "/static/css/main.1de46aec.chunk.css"
+  },
+  {
+    "revision": "fbf1a8c527c6bf1f0183",
+    "url": "/static/js/2.0d347bd9.chunk.js"
+  },
+  {
+    "revision": "a8ced5b9ac83722e7885",
+    "url": "/static/js/main.8f22e9b1.chunk.js"
+  },
+  {
+    "revision": "51810371bf2cf35f1e22",
+    "url": "/static/js/runtime-main.85498786.js"
+  }
+]);
